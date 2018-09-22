3ce1e91d925b84a36cd2565c3fd1f64d80218dd4
Aditya Pillai <github@pillai.io>
2018-09-22 12:07:35 -0400

    CHanged colors

--- app/parser.py
+++ app/parser.py
@@ -35,7 +35,7 @@ def parse(events):
         result['first_entry'] = first_entry.strftime('%H:%M:%S, %b %d %Y')
         result['last_exit'] = last_exit.strftime('%H:%M:%S, %b %d %Y')
         result['time_in_class'] = str(tic / 60) + " minutes"
-        result['present'] = "Present" if tic > 30 * 60 else "Absent"
+        result['present'] = "Present" if tic > 1 * 60 else "Absent"
 
         results.append(result)
 
