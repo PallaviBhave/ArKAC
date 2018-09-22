ceea4f5b9a5c88865aabd09e12ee89f162d9cdd9
Sayan Chaudhry <sayan.chaudhry@gmail.com>
2018-09-22 15:44:31 -0400

    final changes

--- facial_recognition/incam.py
+++ facial_recognition/incam.py
@@ -60,17 +60,17 @@ while True:
             print("No match found")
 
         # Draw a box around the face
-        # cv2.rectangle(frame, (left, top), (right, bottom), (0, 0, 255), 2)
+        cv2.rectangle(frame, (left, top), (right, bottom), (0, 0, 255), 2)
 
         # Draw a label with a name below the face
-        # cv2.rectangle(frame, (left, bottom - 35), (right, bottom), (0, 0, 255), cv2.FILLED)
-        # font = cv2.FONT_HERSHEY_DUPLEX
-        # cv2.putText(frame, name, (left + 6, bottom - 6), font, 1.0, (255, 255, 255), 1)
+        cv2.rectangle(frame, (left, bottom - 35), (right, bottom), (0, 0, 255), cv2.FILLED)
+        font = cv2.FONT_HERSHEY_DUPLEX
+        cv2.putText(frame, name + " OUT", (left + 6, bottom - 6), font, 1.0, (255, 255, 255), 1)
 
     # Display the resulting image
-    # cv2.imshow('Video', frame)
+    cv2.imshow('Video', cv2.resize(frame, (0,0), fx=0.75, fy=0.75))
 
-    time.sleep(0.5)
+    # time.sleep(0.5)
 
     # Hit 'q' on the keyboard to quit!
     if cv2.waitKey(1) & 0xFF == ord('q'):
