1af0609e1d5ad678f4ae96e836fe4846c3022e6a
Sayan Chaudhry <sayan.chaudhry@gmail.com>
2018-09-22 11:52:30 -0400

    Added parser.py

---
+++ app/parser.py
@@ -0,0 +1,39 @@
+import datetime
+
+def parse(events):
+    people = { i['name'] for i in events }
+    people = list(people)
+
+    results = []
+
+    for person in people:
+        all_entries = filter(lambda x : x['name' == person], events)
+
+        rem_dupes = []
+        code = 0
+
+        for entry in all_entries:
+            if entry['code'] == code:
+                rem_dupes.append(entry)
+                code = 1 - code
+
+        if rem_dupes[-1]['code'] == 0:
+            rem_dupes = rem_dupes[:-1]
+
+        tic = 0
+        for i in range(0, len(rem_dupes) - 1, 2):
+            tic += rem_dupes[i+1] - rem_dupes[i]
+
+        first_entry = datetime.datetime.fromtimestamp(rem_dupes[0]['time'])
+        last_exit = datetime.datetime.fromtimestamp(rem_dupes[-1]['time'])
+
+        result = {}
+        result['name'] = person
+        result['first_entry'] = first_entry.strftime('%H:%M:%S, %b %d %Y')
+        result['last_exit'] = last_exit.strftime('%H:%M:%S, %b %d %Y')
+        result['time_in_class'] = str(tic / 1000 / 60) + " minutes"
+        result['present'] = "Present" if tic > 45 * 60 * 1000 else "Absent"
+
+        results.append(result)
+
+    return results
